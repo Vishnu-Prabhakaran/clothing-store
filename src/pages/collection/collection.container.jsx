2b6b2c1b9ee50213369bb39e0ebd38896d733fc1
Vishnu-Prabhakaran <vampire.wish@gmail.com>
2020-02-15 17:23:19 +1100

    Query with variables

---
+++ src/pages/collection/collection.container.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Query } from "react-apollo";
+import { gql } from "apollo-boost";
+
+import CollectionPage from "./collection.component";
+import Spinner from "../../components/spinner/spinner.component";
+
+// // // Actual query schema in apollo server // // // // // // // // // // // // // // // // // // /
+// // // ! means must/ mandatory                                                                   //
+// type Query {                                                                                    //
+//     collections: [Collection!]!                                                                 //
+//     collection(id: ID!): Collection                                                             //
+//     getCollectionsByTitle(title: String): Collection                                            //
+//   }                                                                                             //
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // //  //  //  // //
+
+// **Define the query and the type it is expecting**
+// Make Query with the query from the apollo server getCollectionsByTitle($title: String!) is same as the query defined in the server
+// query returns the object as $title
+// $title is the way to pass parameters for the query
+const GET_COLLECTION_BY_TITLE = gql`
+  query getCollectionsByTitle($title: String!) {
+    getCollectionsByTitle(title: $title) {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
+
+const CollectionPageContainer = ({ match }) => (
+  <Query
+    query={GET_COLLECTION_BY_TITLE}
+    variables={{ title: match.params.collectionId }}
+  >
+    {// destructure the data as it is returning data.getCollectionsByTitle
+    ({ loading, data: { getCollectionsByTitle } }) => {
+      if (loading) return <Spinner />;
+      return <CollectionPage collection={getCollectionsByTitle} />;
+    }}
+  </Query>
+);
+
+export default CollectionPageContainer;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-// instead of changing 'CollectionsOverview' name to 'CollectionsOverviewContainer', "deafault as" keep the alias for CollectionsOverviewContainer as CollectionsOverview.
-import {default as CollectionsOverview} from '../../components/collections-overview/collections-overview.container';
-import CollectionPage from '../collection/collection.component';
+import React from "react";
+import { Route } from "react-router-dom";
+
+// Instead of changing 'CollectionsOverview' name to 'CollectionsOverviewContainer', "deafault as" keep the alias for CollectionsOverviewContainer as CollectionsOverview.
+import { default as CollectionsOverview } from "../../components/collections-overview/collections-overview.container";
+import { default as CollectionPage } from "../collection/collection.container";
 
 const ShopPage = ({ match }) => (
-  <div className='shop-page'>
+  <div className="shop-page">
     <Route exact path={`${match.path}`} component={CollectionsOverview} />
     <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
